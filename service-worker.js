c02bb7c1571452bea230a7d80114d8e0c7b82230
kostya_dyachenko <kd@newagesol.com>
2019-06-22 01:44:37 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/countdown/precache-manifest.41e416bace586d819788ad2096bd37fa.js"
+  "/countdown/precache-manifest.bbb88bf6ee92fc797e3047ea31422312.js"
 );
 
 self.addEventListener('message', (event) => {
