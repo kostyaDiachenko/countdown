490c936f912d4bb1b4beb33293cf55a8e327b21e
kostya_dyachenko <kd@newagesol.com>
2019-06-23 00:01:34 +0300

    Updates

---
+++ precache-manifest.674098bba50a3d5d5c0baee34be1e357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0e001a507a33d1a08cb783cc269229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae9ffbb84298f4c22ae",
+    "url": "/static/css/main.3c2628ac.chunk.css"
+  },
+  {
+    "revision": "3f749cc5ae2f3d7d07e9",
+    "url": "/static/js/2.94789f2c.chunk.js"
+  },
+  {
+    "revision": "7ae9ffbb84298f4c22ae",
+    "url": "/static/js/main.f99ec60e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
